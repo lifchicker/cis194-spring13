3b771abc2ff40f9b24d00aa9a9e7d685c900d5b5
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2018-12-30 17:35:46 +0800

    Lesson03: removed redundant cases for skips

--- Lesson03/src/Cis194/Lesson03/Golf.hs
+++ Lesson03/src/Cis194/Lesson03/Golf.hs
@@ -5,6 +5,4 @@ module Cis194.Lesson03.Golf where
 import Data.List
 
 skips :: [a] -> [[a]]
-skips [] = []
-skips [x] = [[x]]
 skips xs = [([a | (i, a) <- (zip [1..(length xs)] xs), i `mod` j == 0]) | j <- [1..(length xs)]]
