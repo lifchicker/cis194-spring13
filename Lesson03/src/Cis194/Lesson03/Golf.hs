422b682e63cc2a9ea85b42b810babcb7d526b95f
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2018-12-30 17:12:57 +0800

    Lesson03: Shorter version of skips

--- Lesson03/src/Cis194/Lesson03/Golf.hs
+++ Lesson03/src/Cis194/Lesson03/Golf.hs
@@ -4,17 +4,10 @@ module Cis194.Lesson03.Golf where
 
 import Data.List
 
-mapIndex :: [a] -> [(Int, a)]
-mapIndex [] = []
-mapIndex xs = zip [1..(length xs)] xs
-
 genList j (i, a) = if i `mod` j == 0 then [a] else []
-
-genLists :: [a] -> [(Int, a)] -> [[[a]]]
-genLists xs ys = map (\x -> (map (genList x) ys)) [1..(length xs)]
 
 -- for each index i collect all the letters with index j where j/i==0
 skips :: [a] -> [[a]]
 skips [] = []
 skips [x] = [[x]]
-skips xs = map (\x -> concat x) (genLists xs (mapIndex xs))
+skips xs = [(concat (map (genList x) (zip [1..(length xs)] xs))) | x <- [1..(length xs)]]
