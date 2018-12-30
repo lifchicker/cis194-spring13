65f1835fc793d173495ecb41ba8cbe58e1c3ab68
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2018-12-30 17:33:35 +0800

    Lesson03: Shortest skips I could come up with

--- Lesson03/src/Cis194/Lesson03/Golf.hs
+++ Lesson03/src/Cis194/Lesson03/Golf.hs
@@ -4,10 +4,7 @@ module Cis194.Lesson03.Golf where
 
 import Data.List
 
-genList j (i, a) = if i `mod` j == 0 then [a] else []
-
--- for each index i collect all the letters with index j where j/i==0
 skips :: [a] -> [[a]]
 skips [] = []
 skips [x] = [[x]]
-skips xs = [(concat (map (genList x) (zip [1..(length xs)] xs))) | x <- [1..(length xs)]]
+skips xs = [([a | (i, a) <- (zip [1..(length xs)] xs), i `mod` j == 0]) | j <- [1..(length xs)]]
