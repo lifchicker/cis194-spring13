7f6861e28f28e49607ee12bee301caedce4f772c
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2019-01-01 22:36:27 +0800

    Lesson03: [histogram] first working version

--- Lesson03/src/Cis194/Lesson03/Golf.hs
+++ Lesson03/src/Cis194/Lesson03/Golf.hs
@@ -12,5 +12,14 @@ skips xs = [([a | (i, a) <- (zip [1..n] xs), i `mod` j == 0]) | let n = length x
 
 -- create a tuples with (i, i+1, i+2) elements from original list
 -- filter elements which is local maximums
-localMaxima :: [Integer] -> [Integer]
+localMaxima :: [Int] -> [Int]
 localMaxima xs = [b | (a, b, c) <- zip3 xs (tail xs) (tail (tail xs)), (a < b) && (b > c)]
+
+-- for each number, calculate how many of them in input array
+-- starting from max value go throug all elements and compare
+--   if the element appear >= value times, then print "*"
+--   otherwise print " "
+counts xs = [length (filter (\x -> x == i) xs) | i <- [0..9]]
+rows xs = [map (\x -> if x >= i then "*" else " ") (counts xs) | i <- (reverse [1..(maximum (counts xs))])]
+histogram :: [Int] -> String
+histogram xs = intercalate "\n" ([concat $ reverse $ dropWhile (== " ") (reverse r) | r <- (rows xs)] ++ ["=========="] ++ ["0123456789"])
--- Lesson03/test/Cis194/Lesson03/GolfSpec.hs
+++ Lesson03/test/Cis194/Lesson03/GolfSpec.hs
@@ -28,3 +28,9 @@ spec = do
       localMaxima [2,9,5,6,1] `shouldBe` [9,6]
       localMaxima [2,3,4,1,5] `shouldBe` [4]
       localMaxima [1,2,3,4,5] `shouldBe` []
+  describe "histogram" $ do
+    it "takes as input a list of Integers between 0 and 9 (inclusive),\
+    \ and outputs a vertical histogram showing how many of each number\
+    \ were in the input list" $ do
+      histogram [1,1,1,5] `shouldBe` " *\n *\n *   *\n==========\n0123456789"
+      histogram  [1,4,5,4,6,6,3,4,2,4,9] `shouldBe` "    *\n    *\n    * *\n ******  *\n==========\n0123456789"
