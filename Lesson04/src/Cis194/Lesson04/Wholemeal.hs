d79b8bfb974ea55c12cd7320d56ae9a84d1eea7e
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2019-01-12 16:32:42 +0800

    Lesson04: Done with fun1' and added first working version of fun2'

--- Lesson04/src/Cis194/Lesson04/Wholemeal.hs
+++ Lesson04/src/Cis194/Lesson04/Wholemeal.hs
@@ -8,8 +8,15 @@ fun1 (x:xs)
   | even x = (x - 2) * fun1 xs
   | otherwise = fun1 xs
 
+fun1' :: [Integer] -> Integer
+fun1' = foldr (\x y -> (x-2)*y) 1 . filter even
+
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n 
-  | even n = n + fun2 (n `div` 2)
-  | otherwise = fun2 (3 * n + 1)
+  | even n = n + fun2(n `div` 2)
+  | otherwise = fun2(3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' 1 = 0
+fun2' n = (foldr (+) 0 (takeWhile (even) $ iterate (\x -> x `div` 2) n)) + fun2' (3*(head . filter (/=1) . dropWhile (even) $ iterate (\x -> x `div` 2) n) + 1)
--- Lesson04/test/Cis194/Lesson04/WholemealSpec.hs
+++ Lesson04/test/Cis194/Lesson04/WholemealSpec.hs
@@ -12,26 +12,13 @@ module Cis194.Lesson04.WholemealSpec (main, spec) where
   
   spec :: Spec
   spec = do
-    describe "skips" $ do
-      it "The output of skips is a list of lists. The first list in the output should\
-      \ be the same as the input list. The second list in the output should\
-      \ contain every second element from the input list. . . and the nth list in\
-      \ the output should contain every nth element from the input list." $ do
-        skips "ABCD" `shouldBe` ["ABCD", "BD", "C", "D"]
-        skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
-        skips [1] `shouldBe` [[1]]
-        skips [True,False] `shouldBe` [[True,False], [False]]
-        -- skips [] `shouldBe` []
-    describe "localMaxima" $ do
-      it "A local maximum of a list is an element of the list which is strictly\
-      \ greater than both the elements immediately before and after it" $ do
-        localMaxima [2,9,5,6,1] `shouldBe` [9,6]
-        localMaxima [2,3,4,1,5] `shouldBe` [4]
-        localMaxima [1,2,3,4,5] `shouldBe` []
-    describe "histogram" $ do
-      it "takes as input a list of Integers between 0 and 9 (inclusive),\
-      \ and outputs a vertical histogram showing how many of each number\
-      \ were in the input list" $ do
-        histogram [1,1,1,5] `shouldBe` " *        \n *        \n *   *    \n==========\n0123456789"
-        histogram  [1,4,5,4,6,6,3,4,2,4,9] `shouldBe` "    *     \n    *     \n    * *   \n ******  *\n==========\n0123456789"
-  
+    describe "fun1'" $ do
+      it "Should have the same output as fun1" $ do
+        fun1' [1,3,4,5,6,7,8] `shouldBe` fun1 [1,3,4,5,6,7,8]
+    describe "fun2'" $ do
+      it "Should have the same output as fun2" $ do
+        fun2' 1 `shouldBe` fun2 1
+        fun2' 2 `shouldBe` fun2 2
+        fun2' 3 `shouldBe` fun2 3
+        fun2' 4 `shouldBe` fun2 4
+        fun2' 5 `shouldBe` fun2 5
