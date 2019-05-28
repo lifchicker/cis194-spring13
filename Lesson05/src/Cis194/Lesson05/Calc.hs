77b6aa7e94a7c1d18f2f8773663f4d3e2f8f34aa
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2019-05-28 20:51:23 +0800

    Implemented eval function

--- Lesson05/src/Cis194/Lesson05/Calc.hs
+++ Lesson05/src/Cis194/Lesson05/Calc.hs
@@ -4,5 +4,13 @@ module Cis194.Lesson05.Calc where
 
 import Cis194.Lesson05.ExprT
 
+class Evaluable e where
+  calc :: e -> Integer
+
+instance Evaluable ExprT where
+  calc (Lit x) = x
+  calc (Add x y) = (eval x) + (eval y)
+  calc (Mul x y) = (eval x) * (eval y)
+
 eval :: ExprT -> Integer
-eval = undefined
+eval = calc
