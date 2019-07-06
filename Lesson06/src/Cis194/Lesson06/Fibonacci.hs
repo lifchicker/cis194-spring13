cf991956957e3aa407f01efa59e0161e9651037f
Aleksei 'Alex' Antonov <alekseia@seekasia.com>
2019-07-06 16:28:58 +0800

    Implemented fibs2

--- Lesson06/src/Cis194/Lesson06/Fibonacci.hs
+++ Lesson06/src/Cis194/Lesson06/Fibonacci.hs
@@ -2,6 +2,8 @@
 
 module Cis194.Lesson06.Fibonacci where
 
+import Data.List
+
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -9,3 +11,9 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = [fib n | n <- [0..] ]
+
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : (fib2 0 1)
+  where 
+    fib2 x y = xy : fib2 y xy
+      where xy = x + y
--- Lesson06/test/Cis194/Lesson06/FibonacciSpec.hs
+++ Lesson06/test/Cis194/Lesson06/FibonacciSpec.hs
@@ -17,4 +17,7 @@ spec = do
       fib 10 `shouldBe` 55
   describe "fibs1" $ do
     it "defines the infinite list of all Fibonacci numbers" $ do
-      take 11 fibs1 `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+      take 30 fibs1 `shouldBe` [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229]
+  describe "fibs2" $ do
+    it "more efficient implementation of infinite list of all Fibonacci numbers" $ do
+      take 11 fibs2 `shouldBe` take 11 fibs1
