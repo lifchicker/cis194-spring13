969cab249337db772877c4a944245dd8b23e471e
Aleksei 'Alex' Antonov <alekseia@seekasia.com>
2019-08-16 15:05:51 +0800

    Implemented (+++) for Exercise 1

--- Lesson07/src/Cis194/Lesson07/JoinList.hs
+++ Lesson07/src/Cis194/Lesson07/JoinList.hs
@@ -3,3 +3,16 @@
 module Cis194.Lesson07.JoinList where
 
 import Data.List
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) l r = Append (mappend (tag l) (tag r)) l r
