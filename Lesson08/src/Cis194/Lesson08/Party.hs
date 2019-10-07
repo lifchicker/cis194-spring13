ef6ad9b65b34c4a53febb6ae74f61effbf60270c
Alexey 'lifchicker' Antonov <1ifeh4ck3r@gmail.com>
2019-10-07 22:29:54 +0800

    WIP: Lesson08

--- Lesson08/src/Cis194/Lesson08/Party.hs
+++ Lesson08/src/Cis194/Lesson08/Party.hs
@@ -4,20 +4,31 @@ module Cis194.Lesson08.Party where
 import Data.Monoid
 import Data.Semigroup  
 
+import Data.Tree
+
 import Cis194.Lesson08.Employee
 
 glCons :: Employee -> GuestList -> GuestList
-glCons e@(Emp { empFun = ef }) gl@(GL xs x) = GL (e:xs) (ef + x)
+glCons e@(Emp { empFun = ef }) (GL xs x) = GL (e:xs) (ef + x)
 
 instance Semigroup GuestList where 
-  -- can't come up with anything "correct" right now, will leave till later moment when it'll be used
-  -- two options: 
-  -- 1. just add all element from one to another (wrong)
-  -- 2. add without duplicates, re-calculate fun (because newly added elements might reduce fun)
-  (<>) = undefined
+  (<>) (GL lxs ltf) (GL rxs rtf) = GL (lxs ++ rxs) (ltf + rtf)
 
 instance Monoid GuestList where
   mempty = GL [] 0
 
 moreFun :: GuestList -> GuestList -> GuestList
 moreFun l@(GL _ le) r@(GL _ re) = if le > re then l else r
+
+treeFold :: (a -> b -> b) -> b -> Tree a -> b
+treeFold = undefined
+
+combineGLs :: Employee -> [GuestList] -> GuestList
+combineGLs e@(Emp { empFun = ef }) = foldr mappend (GL [e] ef)
+
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e@(Emp { empFun = ef }) = foldr (\(llgl, lrgl) (rlgl, rrgl) -> (mappend llgl rlgl, mappend lrgl rrgl)) ((GL [e] ef), mempty)
+
+maxFun :: Tree Employee -> GuestList
+maxFun xs = moreFun (fst gl) (snd gl)
+  where gl = treeFold nextLevel (mempty, mempty) xs
--- Lesson08/test/Cis194/Lesson08/PartySpec.hs
+++ Lesson08/test/Cis194/Lesson08/PartySpec.hs
@@ -2,18 +2,59 @@ module Cis194.Lesson08.PartySpec (main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
-      
+
+import Data.Tree
+
 import Cis194.Lesson08.Party
 import Cis194.Lesson08.Employee
   
 main :: IO ()
 main = hspec spec
-  
+
+-- A small company hierarchy to use for testing purposes.
+testCompany :: Tree Employee
+testCompany
+  = Node (Emp "Stan" 9)
+    [ Node (Emp "Bob" 2)
+      [ Node (Emp "Joe" 5)
+        [ Node (Emp "John" 1) []
+        , Node (Emp "Sue" 5) []
+        ]
+      , Node (Emp "Fred" 3) []
+      ]
+    , Node (Emp "Sarah" 17)
+      [ Node (Emp "Sam" 4) []
+      ]
+    ]
+
+testCompany2 :: Tree Employee
+testCompany2
+  = Node (Emp "Stan" 9)
+    [ Node (Emp "Bob" 3) -- (8, 8)
+      [ Node (Emp "Joe" 5) -- (5, 6)
+        [ Node (Emp "John" 1) [] -- (1, 0)
+        , Node (Emp "Sue" 5) [] -- (5, 0)
+        ]
+      , Node (Emp "Fred" 3) [] -- (3, 0)
+      ]
+    , Node (Emp "Sarah" 17) -- (17, 4)
+      [ Node (Emp "Sam" 4) [] -- (4, 0)
+      ]
+    ]
+
 spec :: Spec
 spec = do
+  let empA = Emp "A" 1
+  let empB = Emp "B" 2
   describe "glCons" $ do
     it "adds an Employee to the GuestList (updating the cached Fun score appropriately)" $ do
-      glCons (Emp "A" 1) (GL [] 0) `shouldBe` GL [(Emp "A" 1)] 1
+      glCons empA (GL [] 0) `shouldBe` GL [empA] 1
   describe "moreFun" $ do
-    it " takes two GuestLists and returns whichever one of them is more fun, i.e. has the higher fun score" $ do
-      moreFun (GL [(Emp "A" 1)] 1) (GL [(Emp "A" 1), (Emp "B" 1)] 2) `shouldBe` (GL [(Emp "A" 1), (Emp "B" 1)] 2)
+    it "takes two GuestLists and returns whichever one of them is more fun, i.e. has the higher fun score" $ do
+      moreFun (GL [empA] 1) (GL [empA, empB] 3) `shouldBe` (GL [empA, empB] 3)
+  describe "combineGLs" $ do
+    it "employee (the boss of some division) and the optimal guest list for each subdivision under him" $ do
+      combineGLs empA [(GL [empB] 2)] `shouldBe` GL [empB, empA] 3
+  describe "maxFun" $ do
+    it "which takes a company hierarchy as input and outputs a fun-maximizing guest list" $ do
+      maxFun testCompany `shouldBe` GL [] 0
